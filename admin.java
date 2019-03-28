74cb85b25cf9c5d6ec6608ea99cb1e27f5ccccae
parvathybinu <36987696+parvathybinu@users.noreply.github.com>
2019-03-27 23:05:33 -0700

    Add files via upload

---
+++ admin.java
@@ -0,0 +1,27 @@
+package bean;
+
+
+
+public class admin {
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String name,password;
+
+}
